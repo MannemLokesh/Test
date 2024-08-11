4e8277bb31baddd08538745743fc5749b02355d6
MannemLokesh <lokesh.mannem.padma143@gmail.com>
2024-08-11 13:11:37 +0530

    updated ui and added docs

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu-title').addEventListener('click', function(e) {
-    e.preventDefault(); // Prevent the default anchor action
-    const javaContent = document.getElementById('java-content');
-    if (javaContent.style.display === 'flex') {
-        javaContent.style.display = 'none';
-    } else {
-        javaContent.style.display = 'flex';
-    }
-});
